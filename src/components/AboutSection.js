a73c62605d7d6db711de164bbd4add9cff50cfde
Victor Mascarenhas Reis da Silva <victor.mascarenhas@al.infnet.edu.br>
2021-02-24 01:17:29 -0300

    Styling Services Section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,5 @@
 import home1 from '../img/home1.png'
-import styled from 'styled-components'
+import {About, Description, Hide, Image} from '../styles'
 
 const AboutSection = () => {
     return(
@@ -30,29 +30,3 @@ const AboutSection = () => {
 }
 
 export default AboutSection
-
-
-const About = styled.div`
-min-height: 90vh;
-display: flex;
-align-items: center;
-justify-content: space-between;
-padding: 5rem 10rem;
-color: white;
-`
-const Description = styled.div`
-flex: 1;
-padding-right: 5rem;
-`
-const Image = styled.div`
-flex: 1;
-overflow: hidden;
-img{
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-}
-`
-const Hide = styled.div`
-overflow: hidden;
-`
