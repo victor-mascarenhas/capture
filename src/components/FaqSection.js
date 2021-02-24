c26114802e3a72d0d64b1b8cfd075c962783c6c8
Victor Mascarenhas Reis da Silva <victor.mascarenhas@al.infnet.edu.br>
2021-02-24 01:28:27 -0300

    Styling Faq Section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,6 +1,10 @@
+import styled from 'styled-components'
+import {About} from '../styles'
+
+
 const FaqSection = () => {
     return (
-        <div className="faq">
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
@@ -8,6 +12,7 @@ const FaqSection = () => {
                     <p>scelerisque ut, porta vel metus. Proin velit quam, imperdiet vitae euismod in, suscipit in libero. 
                         Nunc congue lectus sit amet diam efficitur efficitur.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
@@ -15,6 +20,7 @@ const FaqSection = () => {
                     <p>scelerisque ut, porta vel metus. Proin velit quam, imperdiet vitae euismod in, suscipit in libero. 
                         Nunc congue lectus sit amet diam efficitur efficitur.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Diferrent Payment Methods</h4>
@@ -22,6 +28,7 @@ const FaqSection = () => {
                     <p>scelerisque ut, porta vel metus. Proin velit quam, imperdiet vitae euismod in, suscipit in libero. 
                         Nunc congue lectus sit amet diam efficitur efficitur.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What Products do you offer</h4>
@@ -29,9 +36,37 @@ const FaqSection = () => {
                     <p>scelerisque ut, porta vel metus. Proin velit quam, imperdiet vitae euismod in, suscipit in libero. 
                         Nunc congue lectus sit amet diam efficitur efficitur.</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </Faq>
     )
 }
 
 export default FaqSection
+
+const Faq = styled(About)`
+display: block;
+span{
+    display: block;
+}
+h2{
+    padding-bottom: 2rem;
+    font-weight: lighter;
+}
+.faq-line{
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%
+}
+.question{
+    padding: 3rem 0rem;
+    cursor: pointer;    
+}
+.answer{
+    padding: 2rem 0rem;
+    p{
+        padding: 1rem 0rem;
+    }
+}
+`
