cacf1d2ed2612b9baab11312cdb3085477b1cc0a
Victor Mascarenhas Reis da Silva <victor.mascarenhas@al.infnet.edu.br>
2021-02-25 17:29:10 -0300

    About Animation

--- src/animation.js
+++ src/animation.js
@@ -1,20 +1,60 @@
 export const pageAnimation = {
-    hidden: {
-        opacity: 0,
-        y: 300
+  hidden: {
+    opacity: 0,
+    y: 300,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      when: "beforeChildren",
+      staggerChildren: 0.25,
     },
-    show:{
-        opacity: 1,
-        y: 0,
-        transition: {
-            duration: 1,
-        },
+  },
+  exit: {
+    opacity: 0,
+    y: 300,
+    transition: {
+      duration: 0.5,
     },
-    exit:{
-        opacity: 0,
-        y: 300,
-        transition: {
-            duration: 1,
-        },        
-    }
+  },
+};
+
+export const titleAnim = {
+  hidden: {
+    y: 200,
+  },
+  show: {
+    y: 0,
+    transition: {
+      duration: 0.75,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const fade = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: { ease: "easeOut", duration: 0.75 },
+  },
+};
+
+export const photoAnim = {
+  hidden: {
+    scale: 1.5,
+    opacity: 0,
+  },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      ease: "easeOut",
+      duration: 0.75,
+    },
+  },
 };
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,32 +1,41 @@
-import home1 from '../img/home1.png'
-import {About, Description, Hide, Image} from '../styles'
+import home1 from "../img/home1.png";
+import { About, Description, Hide, Image } from "../styles";
+
+import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
-    return(
-        <About>
-            <Description>
-                <div className="title">
-                    <Hide>
-                        <h2>We work to make</h2>
-                    </Hide>
-                    <Hide>
-                        <h2>your <span>dreams</span> come 
-                        </h2>
-                    </Hide>
-                    <Hide>
-                        <h2>true.</h2>
-                    </Hide>
-                </div>
-                <p>Contact us for any photography or videography ideas that you have.
-                    We have professionals with amazing skils.</p>
-                    <button>Contact Us</button>
-            </Description>
-            <Image>
-                <img src={home1} alt="a simple guy with a camera"/>
-            </Image>
-        </About>
+  return (
+    <About>
+      <Description>
+        <motion.div className="title">
+          <Hide>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>
+              your <span>dreams</span> come
+            </motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
+          </Hide>
+        </motion.div>
+        <motion.p variants={fade}>
+          Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skils.
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
+      </Description>
+      <Image>
+        <motion.img
+          variants={photoAnim}
+          src={home1}
+          alt="a simple guy with a camera"
+        />
+      </Image>
+    </About>
+  );
+};
 
-    )
-}
-
-export default AboutSection
+export default AboutSection;
