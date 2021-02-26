8f831b5a869a8d7bf9171327c786d11849cf372f
Victor Mascarenhas Reis da Silva <victor.mascarenhas@al.infnet.edu.br>
2021-02-26 02:54:31 -0300

    Scroll Animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 import { About } from "../styles";
 import Toggle from "./Toggle";
+import useScroll from "./useScroll";
 import { AnimateSharedLayout } from "framer-motion";
+import { scrollReveal } from "../animation";
 
 const FaqSection = () => {
+  const [element, controls] = useScroll();
   return (
-    <Faq>
+    <Faq
+      variants={scrollReveal}
+      ref={element}
+      animate={controls}
+      initial="hidden"
+    >
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
